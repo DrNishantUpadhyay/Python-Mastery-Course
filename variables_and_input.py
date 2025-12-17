434a8de02c9cba622a0e6bc1297468c146ad6738
Dr Nishant Upadhyay <hs46341999@gmail.com>
2025-12-17 16:16:27 +0530

    Create variables_and_input.py
    The Basics (Variable & Types)

---
+++ variables_and_input.py
@@ -0,0 +1,14 @@
+# Demonstrating basic data types and user input
+name = "Nishant Upadhyay"
+age = 25
+price = 25.99
+
+print(f"Name: {name}, Age: {age}, Type: {type(age)}")
+
+# Basic Arithmetic and Logic
+a = int(input("Enter first number for sum: "))
+b = int(input("Enter second number for sum: "))
+print("Total Sum:", a + b)
+
+# Logical Operators
+print("Not False is:", not False)
